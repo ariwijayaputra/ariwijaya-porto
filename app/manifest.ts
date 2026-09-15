8d4b5342e6a27ad99f746ebccb4855d6cc079832
ariwijayaputra <aryanathaa@gmail.com>
2026-09-16 00:37:28 +0800

    feat: update favicon and manifest

---
+++ app/manifest.ts
@@ -0,0 +1,24 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Ari Wijaya Putra",
+    short_name: "Ari Wijaya",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#121212",
+    theme_color: "#121212",
+    icons: [
+      {
+        src: "/icon/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
